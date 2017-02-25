dcfe3f72bbc936b4d71a44f50b8db7b734abb2d1
Gabriel Zea <zea@gbrl.in>
2017-02-25 11:51:03 -0500

    comentarios y formato en rutas

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
+  // A diferencia de las otras dos rutas en esta aplicación
+  // acá no se ejecuta el método render de la respuesta
+  // por lo tanto la respuesta que se envía al cliente no incluye todo
+  // el documento HTML espcificado en la vista.
   res.send('respond with a resource');
 });
 
